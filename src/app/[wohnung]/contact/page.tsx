c848ecbf533e503720fa9383552190e6cee39550
hplytnikas <h.plytnikas@gmail.com>
2025-02-17 10:14:41 +0100

    minor changes

--- src/app/[wohnung]/contact/page.tsx
+++ src/app/[wohnung]/contact/page.tsx
@@ -3,14 +3,19 @@
 import Navigation from "@/app/components/navigation";
 import styles from "./contact.module.css";
 import { useParams } from "next/navigation";
+import { useTranslation } from "react-i18next";
 
 export default function Page() {
-  const { wohnung } = useParams();
+  const { t } = useTranslation();
+  
   return (
     <>
       <Navigation home={false} />
       <div className={styles.container}>
-        <p> Contact of {wohnung}</p>
+        <h1 className={styles.title}> {t("contact title")}</h1>
+        {/* <p> Contact of {wohnung}</p> */}
+        <p> Tel: </p>
+        <p> Email: </p>
       </div>
     </>
   );
--- src/app/[wohnung]/requestform/page.tsx
+++ src/app/[wohnung]/requestform/page.tsx
@@ -1,7 +1,21 @@
+"use client"
+
 import Navigation from "@/app/components/navigation";
 import styles from "./requestform.module.css";
+import { useParams } from "next/navigation";
 
 export default function Page() {
+    //logic to check which wohnung it is - automatically knows which one it is 
+    const { wohnung } = useParams();
+
+
+    //info for request:
+    /*
+    * Name, Lastname
+    * Email
+    * ??
+    * 
+    */
     return (
         <>
             <Navigation home={false}/>
